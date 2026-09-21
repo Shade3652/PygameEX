4be115b81fc82a3d1711d64e45f61f0cf611cca6
Jacob Carlson <jacobcarlson646@gmail.com>
2026-09-20 23:19:28 -0400

    Add types to all functions

--- Pygame/__init__.py
+++ Pygame/__init__.py
@@ -3,8 +3,8 @@ from .screen_object import *
 from .keyboard import *
 from enum import Enum
 
-class Pygame:
-    def __init__(self, screen_w, screen_h, title = "Pygame Project", clear_color = (0, 0, 0)):
+class PygameEX:
+    def __init__(self, screen_w: int, screen_h: int, title: str = "Pygame Project", clear_color: tuple[int, int, int] = (0, 0, 0)):
         pygame.init()
         self.window = pygame.display.set_mode((screen_w, screen_h))
         pygame.display.set_caption(title)
@@ -16,6 +16,8 @@ class Pygame:
         self.screens = ["Screen 0"]
         self.screen_data = [[]]
         self.screen_interrupts = [{}]
+
+        self.transcendental_elements = []
 
         self.active_screen = 0
         self.event_queue = []
@@ -29,6 +31,8 @@ class Pygame:
         self.window.fill(self.clear_color)
         for element in self.elements:
             element.draw(self.window)
+        for element in self.transcendental_elements:
+            element.draw(self.window)
         pygame.display.flip()
 
         for pygame_event in pygame.event.get():
@@ -36,6 +40,12 @@ class Pygame:
                 event = _Event(pygame_event)
                 if event.type == event_types.LEFT_CLICK:
                     for element in self.elements:
+                        if type(element) == ScreenButton:
+                            (x, y) = event.pos
+                            if element.click_callback != None and element.was_clicked(x, y, self):
+                                element.click_callback()
+
+                    for element in self.transcendental_elements:
                         if type(element) == ScreenButton:
                             (x, y) = event.pos
                             if element.click_callback != None and element.was_clicked(x, y, self):
@@ -49,21 +59,33 @@ class Pygame:
                 #This event is not implemented in the wrapper yet, so just ignore it :D
                 pass
 
-    def set_clear_color(self, color):
+    def set_clear_color(self, color: tuple[int, int, int]):
         self.clear_color = color
 
-    def add_element(self, element):
+    def add_element(self, element: ScreenObject):
         self.elements.append(element)
 
-    def get_element_index(self, element):
+    def remove_element(self, element: ScreenObject):
+        self.elements.pop(self.elements.index(element))
+
+    def add_transcendental_element(self, element: ScreenObject):
+        self.transcendental_elements.append(element)
+
+    def remove_transcendental_element(self, element: ScreenObject):
+        self.transcendental_elements.pop(self.transcendental_elements.index(element))
+
+    def get_element_index(self, element: ScreenObject):
         return self.elements.index(element)
 
-    def move_element_forward(self, element, places = 1):
+    def element_on_screen(self, element: ScreenObject):
+        return element in self.elements or element in self.transcendental_elements
+
+    def move_element_forward(self, element: ScreenObject, places: int = 1):
         element_index = self.elements.index(element)
         element = self.elements.pop(element_index)
         self.elements.insert(element_index + places, element)
 
-    def create_screen(self, name):
+    def create_screen(self, name: str):
         self.screens.append(name)
         self.screen_data.append([])
         self.screen_interrupts.append({"keyboard": keyboard.init_interrupt_dict()})
@@ -71,7 +93,7 @@ class Pygame:
     def get_current_screen(self):
         return self.screens[self.active_screen]
 
-    def swap_screens(self, new_screen_name):
+    def swap_screens(self, new_screen_name: str):
         self.screen_data[self.active_screen] = self.elements
         self.screen_interrupts[self.active_screen] = self.external_interrupts
 
@@ -80,7 +102,7 @@ class Pygame:
         self.elements = self.screen_data[self.active_screen]
         self.external_interrupts = self.screen_interrupts[self.active_screen]
 
-    def destroy_screen(self, screen_name):
+    def destroy_screen(self, screen_name: str):
         current_screen_name = self.screens[self.active_screen]
         screen_to_pop = self.screens.index(screen_name)
         self.screens.pop(screen_to_pop)
@@ -88,10 +110,13 @@ class Pygame:
         self.screen_interrupts.pop(screen_to_pop)
         self.active_screen = self.screens.index(current_screen_name)
 
+    def screen_exists(self, screen_name: str):
+        return screen_name in self.screens
+
     def get_mouse_pos(self):
         return pygame.mouse.get_pos()
 
-    def register_keyboard_interrupt(self, key, function):
+    def register_keyboard_interrupt(self, key: keyboard.Keys, function):
         self.external_interrupts["keyboard"][key] = function
 
 
@@ -106,10 +131,10 @@ class event_types(Enum):
 
 #Prefixed with an underscore so that if you run "from Pygame import *", it isn't auto imported.
 class _Event:
-    def __init__(self, type_):
+    def __init__(self, type_: pygame.event):
         self.type = self.assign_event_type(type_)
 
-    def assign_event_type(self, pygame_event):
+    def assign_event_type(self, pygame_event: pygame.event):
         one_to_one = [pygame.QUIT]
         processing =           [pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN]
         processing_functions = [self.__mouse_button,    self.__key_press]
@@ -120,7 +145,7 @@ class _Event:
         else:
             return processing_functions[processing.index(pygame_event.type)](pygame_event)
 
-    def __mouse_button(self, event):
+    def __mouse_button(self, event: event_types):
 
         self.pos = event.pos
 
@@ -131,7 +156,7 @@ class _Event:
         elif event.button == 3:
             return event_types("right_click")
 
-    def __key_press(self, event):
+    def __key_press(self, event: event_types):
             self.key = keyboard.assign_key(event.key)
 
             return event_types("key_press")
--- Pygame/screen_object.py
+++ Pygame/screen_object.py
@@ -1,4 +1,10 @@
+from __future__ import annotations  # 1. MUST BE LINE 1 OF THE FILE
+from typing import TYPE_CHECKING
 import pygame
+
+if TYPE_CHECKING:   #This is a step that comes after python evaluates all imports but right before it type checks, so it'll import the class JUST in time for use!
+    from Pygame import PygameEX
+
 
 class ScreenObject():
     def __init__(self, x, y, width, height):
@@ -19,14 +25,18 @@ class ScreenObject():
     def get_font(self):
         return self.font.name
 
-    def update_pos(self, new_x, new_y):
+    def update_pos(self, new_x: int, new_y: int):
         self.rect = pygame.Rect(new_x, new_y, self.get_width(), self.get_height())
-    def update_wh(self, new_w, new_h):
+    def update_wh(self, new_w: int, new_h: int):
             self.rect = pygame.Rect(self.rect.left, self.rect.top, new_w, new_h)
+
+    
+    def was_clicked(self, x: int, y: int, window: PygameEX):
+        return  self.rect.collidepoint((x, y)) and window.element_on_screen(self)
 
 
 class ScreenText(ScreenObject):
-    def __init__(self, text, x, y, text_font = "Arial", text_size = 20, color = (0, 0, 0)):
+    def __init__(self, text: str, x: int, y: int, text_font: str = "Arial", text_size: int = 20, color: tuple[int, int, int] = (0, 0, 0)):
 
         self.font = pygame.font.SysFont(text_font, text_size)
         self.color = color
@@ -38,46 +48,46 @@ class ScreenText(ScreenObject):
         self.text = text
 
 
-    def update_text(self, text):
+    def update_text(self, text: str):
         self.text = text
         (width, height) = self.font.size(text)
         self.rect = pygame.Rect(self.rect.left, self.rect.top, width, height)
 
-    def update_font(self, font):
+    def update_font(self, font: str):
         self.font = pygame.font.SysFont(font, self.text_size)
         (width, height) = self.font.size(self.text)
         self.update_wh(width, height)
 
-    def update_text_size(self, size):
+    def update_text_size(self, size: int):
             self.text_size = size
             self.font = pygame.font.SysFont(self.font.name, self.text_size)
             (width, height) = self.font.size(self.text)
             self.update_wh(width, height)
     
 
-    def draw(self, screen):
+    def draw(self, screen: PygameEX):
         text_surf = self.font.render(self.text, True, self.color)
         text_rect = text_surf.get_rect(center=self.rect.center)
         screen.blit(text_surf, text_rect)
 
 class ScreenImage(ScreenObject):
-    def __init__(self, image_path, x, y):
+    def __init__(self, image_path: str, x: int, y: int):
         self.image = pygame.image.load(image_path)
         (width, height) = self.image.get_size()
 
         super().__init__(x, y, width, height)
 
-    def resize_image(self, width, height):
+    def resize_image(self, width: int, height: int):
         self.image = pygame.transform.scale(self.image, (width, height))
         (width, height) = self.image.get_size()
         self.update_wh(width, height)
 
-    def scale_image(self, scale_factor):
+    def scale_image(self, scale_factor: float):
             self.image = pygame.transform.scale(self.image, (int(self.get_width() * scale_factor), int(self.get_height() * scale_factor)))
             (width, height) = self.image.get_size()
             self.update_wh(width, height)
 
-    def draw(self, screen):
+    def draw(self, screen: PygameEX):
         screen.blit(self.image, (self.get_x(), self.get_y()))
 
 
@@ -85,7 +95,8 @@ class ScreenImage(ScreenObject):
 class ScreenButton(ScreenObject):
 
     #Initializes a button with the desired parameretrs & flags
-    def __init__(self, x, y, width, height, text, text_size = 20, text_font = "Arial", text_color = (0, 0, 0), color = (255, 255, 255), click_callback = None):
+    def __init__(self, x: int, y: int, width: int, height: int, text: str, text_size: int = 20, 
+                 text_font: str = "Arial", text_color: tuple[int, int, int] = (0, 0, 0), color: tuple[int, int, int] = (255, 255, 255), click_callback = None):
         #This is all basic object init stuff
         super().__init__(x, y, width, height)
         self.text = text
@@ -100,11 +111,8 @@ class ScreenButton(ScreenObject):
         self.font = pygame.font.SysFont(text_font, text_size)
 
     #Draws the button on the given surface
-    def draw(self, screen):
+    def draw(self, screen: PygameEX):
         pygame.draw.rect(screen, self.color, self.rect)
         text_surf = self.font.render(self.text, True, self.text_color)
         text_rect = text_surf.get_rect(center=self.rect.center)
         screen.blit(text_surf, text_rect)
-
-    def was_clicked(self, x, y, window):
-        return  self.rect.collidepoint((x, y)) and self in window.elements
